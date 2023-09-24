41fb552c684bafe6c716605621570422bf651089
Rahem <sorourrahem17@gmail.com>
2023-09-24 23:41:32 +0100

    completed assignement

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+
+export default function EyesOneMe(){
+    function handelFocus(){
+        console.log("Good!")
+    }
+    function handelBlur(){
+        console.log('Hey! Eyes on me!')
+    }
+    return(
+        <div>
+            <button onFocus={handelFocus} onBlur={handelBlur}>Eyes on me</button>
+        </div>
+    )
+}
